922a034132e4efe0cc405951ede8ae053556b574
RENAD <RenadAnwar65@gmail.com>
2025-04-30 20:07:09 +0300

    Update user_data_script.sh

--- user_data_script.sh
+++ user_data_script.sh
@@ -3,5 +3,4 @@ yum update -y
 yum install nginx -y
 systemctl start nginx
 systemctl enable nginx
-aws s3 cp s3://renad-clarusway-assets/index.html
-/usr/share/nginx/html/index.html
+aws s3 cp s3://renad-clarusway-assets/index.html /usr/share/nginx/html/index.html
